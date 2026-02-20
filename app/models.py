9ef79833df024c0febdd8da3a16aa4013543f776
omatie <omatie.persad2004@gmail.com>
2026-02-20 15:30:33 -0400

    lab 4 in class

--- app/models.py
+++ app/models.py
@@ -2,6 +2,20 @@ from sqlmodel import Field, SQLModel, Relationship
 from typing import Optional
 from pydantic import EmailStr   #insert at top of the file
 
+class Token(SQLModel):
+    access_token: str
+    token_type: str
+
+class UserCreate(SQLModel):
+    username:str
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=8, max_length=128)
+
+class UserResponse(SQLModel):
+    id: Optional[int]
+    username:str
+    email: EmailStr
+
 class User(SQLModel, table=False):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True)
@@ -27,6 +41,18 @@ class Category(SQLModel, table=True):
     text:str
 
     todos:list['Todo'] = Relationship(back_populates="categories", link_model=TodoCategory)
+
+class TodoCreate(SQLModel):
+    text:str
+
+class TodoResponse(SQLModel):
+    id: Optional[int] = Field(primary_key=True, default=None)
+    text:str
+    done: bool = False
+
+class TodoUpdate(SQLModel):
+    text: Optional[str] = None
+    done: Optional[bool] = None
 
 class Todo(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True, default=None)
